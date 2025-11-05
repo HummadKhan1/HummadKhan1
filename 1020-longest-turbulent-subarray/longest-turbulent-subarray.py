1c2ab9433421725d4922db5ea10d4ebfca141cdd
Hummad Khan <96032426+HummadKhan1@users.noreply.github.com>
2025-11-05 13:56:40 -0600

    Time: 69 ms (34.99%) | Memory: 20.3 MB (93.71%) - LeetSync

--- 1020-longest-turbulent-subarray/longest-turbulent-subarray.py
+++ 1020-longest-turbulent-subarray/longest-turbulent-subarray.py
@@ -3,6 +3,7 @@ class Solution:
         L, R = 0, 1
         res = 1
         prev = ""
+
         while R < len(arr):
             if arr[R] > arr[R-1] and prev != ">":
                 res = max(res, R-L+1)
