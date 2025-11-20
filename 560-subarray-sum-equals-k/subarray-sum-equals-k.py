c10506ad68623a58c1cef3e66f92e4badd91a0e5
Hummad Khan <96032426+HummadKhan1@users.noreply.github.com>
2025-11-20 11:56:25 -0600

    Time: 24 ms (90.67%) | Memory: 20.5 MB (22.92%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,13 +1,8 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        '''
-        parameters: array of ints nums, int k.
-        Return: number of subarrays whose sum is k
-        constraints: 
-        '''
         prefixSum = {0:1}
         prefix = 0
-        res = 0
+        res = 0 
         for n in nums:
             prefix += n
             find = prefix-k
@@ -15,6 +10,3 @@ class Solution:
                 res += prefixSum.get(find)
             prefixSum[prefix] = prefixSum.get(prefix, 0)+1
         return res
-
-        
-        
