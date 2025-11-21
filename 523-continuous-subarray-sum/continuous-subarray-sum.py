417f93ddcf0641f38fd3c7a5feff938c02bf9a4a
Hummad Khan <96032426+HummadKhan1@users.noreply.github.com>
2025-11-21 10:41:31 -0600

    Time: 62 ms (20.26%) | Memory: 38.2 MB (14.28%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.py
+++ 523-continuous-subarray-sum/continuous-subarray-sum.py
@@ -1,16 +1,26 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        '''
+        parameters: int arr nums, int k.
+        Return: boolean
+        note: 
+        prefix sums
+        '''
         prefixSum = {0:-1}
         prefix = 0
         for i in range(len(nums)):
             prefix += nums[i]
-            if k != 0:
-                prefix  = prefix % k 
-            
+            prefix = prefix % k
+
+            if prefix == 0:
+                if i >= 2:
+                    return True  
                 
+            
             if prefix in prefixSum:
-                if i-prefixSum[prefix] >= 2:
+                if i-prefixSum[prefix]>= 2:
                     return True
+                
             
             if prefix not in prefixSum:
                 prefixSum[prefix] = i
