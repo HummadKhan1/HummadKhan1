74c6c0334457eeede7a76724075e490e79bc38cb
Hummad Khan <96032426+HummadKhan1@users.noreply.github.com>
2025-11-21 11:50:08 -0600

    Time: 83 ms (47.66%) | Memory: 31.9 MB (87.70%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,16 +1,25 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        L = 0
-        cur_sum = 0
-        max_sum = nums[0]
-        maxL, maxR = 0,0
-        for R in range(len(nums)):
-            if cur_sum < 0:
-                cur_sum = 0
-                L = R
-            cur_sum += nums[R]
-            if cur_sum > max_sum:
-                max_sum = cur_sum
-                maxL, maxR = L, R
-        print(nums[maxL:maxR+1])
-        return max_sum
+        '''
+        parametersL int arr nums.
+        return : sum of the largest subarray.
+        constraints: elements could be negative.
+        edge cases:
+        1. Initialize L, R = 0, 0, curSum, max_sum
+        2. while R < len(nums)
+        3.  if curSum < 0:
+        4.      curSum = 0
+        5.      L = R
+        6.  end loop
+        7. curSum += nums[R]
+        8. maxSum = max(curSum, maxSum)
+        '''
+        curSum = 0
+        maxSum = nums[0]
+        for n in nums:
+            curSum = max(curSum, 0)
+
+            curSum += n
+
+            maxSum = max(maxSum, curSum)
+        return maxSum
