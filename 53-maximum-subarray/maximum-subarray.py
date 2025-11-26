a08e5a09dd9b35fcd6700a0507d88a9bd7276136
Hummad Khan <96032426+HummadKhan1@users.noreply.github.com>
2025-11-26 12:56:02 -0600

    Time: 89 ms (34.55%) | Memory: 32 MB (66.41%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,25 +1,18 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         '''
-        parametersL int arr nums.
-        return : sum of the largest subarray.
-        constraints: elements could be negative.
-        edge cases:
-        1. Initialize L, R = 0, 0, curSum, max_sum
-        2. while R < len(nums)
-        3.  if curSum < 0:
-        4.      curSum = 0
-        5.      L = R
-        6.  end loop
-        7. curSum += nums[R]
-        8. maxSum = max(curSum, maxSum)
+        parameters: int arr nums.
+        return: subarray with largest sum.
+        constraints: nums[i] could be negative.
+        kadane's algorithm
         '''
-        curSum = 0
-        maxSum = nums[0]
+        L = 0
+        max_sum = float('-inf')
+        cur_sum = 0
         for n in nums:
-            curSum = max(curSum, 0)
+            cur_sum = max(cur_sum, 0)
 
-            curSum += n
+            cur_sum += n
 
-            maxSum = max(maxSum, curSum)
-        return maxSum
+            max_sum = max(max_sum, cur_sum)
+        return max_sum
