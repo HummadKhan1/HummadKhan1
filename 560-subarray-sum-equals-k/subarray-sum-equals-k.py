718d5258924d5c612bfd4a62d5f980d07a733a73
Hummad Khan <96032426+HummadKhan1@users.noreply.github.com>
2025-12-02 11:15:17 -0600

    Time: 29 ms (67.85%) | Memory: 20.5 MB (22.51%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,12 +1,19 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        prefixSum = {0:1}
+        '''
+        parameters:int arr nums, int k.
+        return: total number of subarrays where the sum == k.
+        Really asking: 
+        constraints: elements can be negative.
+        '''
+        prefix_sums = {0:1}
         prefix = 0
-        res = 0 
+        res = 0
         for n in nums:
             prefix += n
-            find = prefix-k
-            if find in prefixSum:
-                res += prefixSum.get(find)
-            prefixSum[prefix] = prefixSum.get(prefix, 0)+1
+            diff = prefix - k
+            if diff in prefix_sums:
+                res += prefix_sums[diff]
+            prefix_sums[prefix] = prefix_sums.get(prefix, 0)+1
+            
         return res
