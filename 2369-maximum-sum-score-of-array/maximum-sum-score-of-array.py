45d51d69c1979d83ce4fed516fcabc5a32ce21c6
Hummad Khan <96032426+HummadKhan1@users.noreply.github.com>
2025-12-02 11:51:48 -0600

    Time: 73 ms (36.21%) | Memory: 36.8 MB (12.07%) - LeetSync

--- 2369-maximum-sum-score-of-array/maximum-sum-score-of-array.py
+++ 2369-maximum-sum-score-of-array/maximum-sum-score-of-array.py
@@ -2,8 +2,9 @@ class Solution:
     def maximumSumScore(self, nums: List[int]) -> int:
         '''
         parameters: int arr nums.
-        return: max sum of a subarray.
-        constraints:
+        return: max sum score.
+        Really asking: max_sum = max(max_sum, max(maxLeft, maxRight))
+        constraints: 
         '''
         maxLeft = []
         total = 0
@@ -11,13 +12,13 @@ class Solution:
             total += n
             maxLeft.append(total)
         
-        maxRight = [0]*len(nums)
+        maxRight=[0]*len(nums)
         total = 0
         for i in range(len(nums)-1,-1,-1):
             total += nums[i]
             maxRight[i] = total
+        
         max_sum = nums[0]
         for i in range(len(nums)):
-            cur_sum = max(maxLeft[i], maxRight[i])
-            max_sum = max(max_sum, cur_sum)
+            max_sum = max(max_sum, max(maxLeft[i], maxRight[i]))
         return max_sum
