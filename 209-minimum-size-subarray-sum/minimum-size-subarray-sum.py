7886a9ced8b50649954dc8b022255c92afb8ea63
Hummad Khan <96032426+HummadKhan1@users.noreply.github.com>
2025-12-02 12:16:18 -0600

    Time: 7 ms (92.27%) | Memory: 28.2 MB (95.96%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -1,9 +1,14 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        cur_sum = 0
-        min_len = float("inf")
+        '''
+        Parameters: arr of pos ints nums, pos int target.
+        returns: min len of subarray where sum >= target. Or 0.
+        Really asking: sliding window. 
+        constraints: 
+        '''
         L = 0
-
+        min_len = float("inf")
+        cur_sum = 0
         for R in range(len(nums)):
             cur_sum += nums[R]
             while cur_sum >= target:
