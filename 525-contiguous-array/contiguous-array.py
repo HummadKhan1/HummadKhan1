3aef3954f496c72e2f85e9aef2c9adb44fd56d25
Hummad Khan <96032426+HummadKhan1@users.noreply.github.com>
2025-12-15 13:48:38 -0600

    Time: 104 ms (49.75%) | Memory: 23.3 MB (88.25%) - LeetSync

--- 525-contiguous-array/contiguous-array.py
+++ 525-contiguous-array/contiguous-array.py
@@ -1,24 +1,16 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
-        '''
-        parameters: binary arr nums.
-        return: max len of subarray with an equal number of 0 and 1s.
-        Actually means: 
-        edge cases: 
-        '''
-        prefixSum = {0:-1}
+        prefixSums = {0:-1}
         prefix = 0
-        max_len = 0
         res = 0
         for i in range(len(nums)):
             if nums[i] > 0:
                 prefix += 1
             else:
                 prefix -= 1
-            if prefix in prefixSum:
-                max_len = max(max_len, i-prefixSum[prefix])
+            
+            if prefix in prefixSums:
+                res = max(res, i-prefixSums[prefix])
             else:
-                prefixSum[prefix] = i
-        return max_len
-            
-            
+                prefixSums[prefix] = i
+        return res
