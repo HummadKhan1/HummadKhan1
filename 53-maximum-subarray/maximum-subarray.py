d40752c3c691685cdb5c96e3dccdedd737fad92a
Hummad Khan <96032426+HummadKhan1@users.noreply.github.com>
2025-12-15 12:04:24 -0600

    Time: 92 ms (25.97%) | Memory: 32 MB (66.33%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -3,16 +3,17 @@ class Solution:
         '''
         parameters: int arr nums.
         return: subarray with largest sum.
-        constraints: nums[i] could be negative.
-        kadane's algorithm
+        really asking: if cur_sum < 0: cur_sum = 0
+        constraints: 
         '''
-        L = 0
-        max_sum = float('-inf')
+        max_sum = nums[0]
         cur_sum = 0
+        L = 0
         for n in nums:
             cur_sum = max(cur_sum, 0)
 
             cur_sum += n
 
             max_sum = max(max_sum, cur_sum)
+
         return max_sum
