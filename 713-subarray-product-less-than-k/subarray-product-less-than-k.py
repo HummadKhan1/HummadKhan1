728c8f4c4b51efc506437fc75142b2395ade2ba0
Hummad Khan <96032426+HummadKhan1@users.noreply.github.com>
2025-12-15 14:06:06 -0600

    Time: 48 ms (79.63%) | Memory: 19.8 MB (17.30%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.py
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.py
@@ -2,20 +2,23 @@ class Solution:
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
         '''
         parameters: int arr nums, int k.
-        return: NUMBER OF contiguous subarrays.
-        Really asking: 
+        return: NUMBER OF subarray.
+        Really asking: Sliding WIndow, res += R-L+1
+        constraints:
         '''
         if k == 0:
             return 0
-        
         L = 0
-        curProd = 1
         res = 0
+        curProd = 1
         for R in range(len(nums)):
             curProd *= nums[R]
+            
             while curProd >= k and L <= R:
                 curProd //= nums[L]
                 L += 1
+
             if curProd < k:
-                res += (R-L+1)
+                res += R-L+1
+
         return res
