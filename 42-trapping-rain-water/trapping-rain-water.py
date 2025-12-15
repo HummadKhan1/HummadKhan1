f6e966eafbcccc74d04d254f264809efcafab7df
Hummad Khan <96032426+HummadKhan1@users.noreply.github.com>
2025-12-15 13:31:26 -0600

    Time: 33 ms (13.66%) | Memory: 19.7 MB (7.69%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,26 +1,22 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        '''
-        parameters: int arr height.
-        return: total amount of water.
-        really asking: min(height[L], height[R]) - nums[i]
-        constraints: 
-        '''
         maxLeft = []
-        max_height = 0
+        maxHeight = 0
         for n in height:
-            maxLeft.append(max_height)
-            max_height = max(max_height, n)
+            maxLeft.append(maxHeight)
+            maxHeight = max(maxHeight, n)
         
-        maxRight=[0]*len(height)
-        max_height = 0
+        print(maxLeft)
+        maxRight = [0]*len(height)
+        maxHeight = 0
         for i in range(len(height)-1,-1,-1):
-            maxRight[i] = max_height
-            max_height = max(max_height, height[i])
+            maxRight[i] = maxHeight
+            maxHeight = max(maxHeight, height[i])
+        print(maxRight)
+
         res = 0
         for i in range(len(height)):
-            cur_height = min(maxLeft[i],maxRight[i])-height[i]
-            if cur_height > 0:
-                res += cur_height
+            diff = min(maxLeft[i], maxRight[i]) - height[i]
+            if diff > 0:
+                res += diff
         return res
-            
