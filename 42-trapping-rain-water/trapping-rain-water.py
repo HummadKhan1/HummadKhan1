e6dead505d97ec5f8cbae63c33d292c8c596e99a
Hummad Khan <96032426+HummadKhan1@users.noreply.github.com>
2026-02-10 15:26:59 -0600

    Time: 7 ms (80.24%) | Memory: 21.3 MB (17.73%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -5,18 +5,17 @@ class Solution:
         for n in height:
             maxLeft.append(maxHeight)
             maxHeight = max(maxHeight, n)
-        
-        print(maxLeft)
         maxRight = [0]*len(height)
         maxHeight = 0
         for i in range(len(height)-1,-1,-1):
             maxRight[i] = maxHeight
             maxHeight = max(maxHeight, height[i])
-        print(maxRight)
-
+        
         res = 0
         for i in range(len(height)):
-            diff = min(maxLeft[i], maxRight[i]) - height[i]
+            min_height = min(maxLeft[i], maxRight[i])
+            diff = min_height-height[i]
             if diff > 0:
                 res += diff
+
         return res
