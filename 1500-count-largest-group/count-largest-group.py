f0a4e8aea459d2b5f355792d3fdfc4e12a67786f
Hummad Khan <96032426+HummadKhan1@users.noreply.github.com>
2026-02-16 14:13:41 -0600

    Time: 67 ms (12.18%) | Memory: 20 MB (6.59%) - LeetSync

--- 1500-count-largest-group/count-largest-group.py
+++ 1500-count-largest-group/count-largest-group.py
@@ -1,27 +1,28 @@
 class Solution:
     def countLargestGroup(self, n: int) -> int:
         '''
-        parameters: int n
-        return: NUMBER of groups that have largest size.
-        Really asking: hashmap problem.
-
+        parameters: int n.
+        return: number of groups.
+        Really asking: dictionary grouping problem.
         '''
         group_dict = {}
-        max_len = 0
+        max_size = 0
         res = 0
         for i in range(1, n+1):
-            str_i = str(i)
             total = 0
-            for s in str_i:
-                total += int(s)
+            str_n = str(i)
+            for num in str_n:
+                total += int(num)
+            print(total)
             if total in group_dict:
                 group_dict[total].append(i)
             else:
                 group_dict[total] = [i]
-            max_len = max(max_len, len(group_dict[total]))
+
+            max_size = max(max_size, len(group_dict[total]))
+        
         new_list = list(group_dict.values())
-        for l in new_list:
-            if len(l) == max_len:
+        for group in new_list:
+            if len(group) == max_size:
                 res += 1
         return res
-            
