b2c2c355dba3915bd101af720d07e7814c042efa
Hummad Khan <96032426+HummadKhan1@users.noreply.github.com>
2026-02-16 14:17:58 -0600

    Time: 11 ms (84.16%) | Memory: 21.8 MB (71.32%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,20 +1,10 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        '''
-        parameters: array of strings strs.
-        return: array of the strings grouped together.
-        Really asking: group anagrams problem.
-        1. initialize hashmap.
-        2. for loop
-        3. 
-        '''
         group_dict = {}
-
         for word in strs:
             sorted_word = ''.join(sorted(word))
             if sorted_word in group_dict:
                 group_dict[sorted_word].append(word)
             else:
                 group_dict[sorted_word] = [word]
-        new_list = list(group_dict.values())
-        return new_list
+        return list(group_dict.values())
