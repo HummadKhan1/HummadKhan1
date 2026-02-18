17c2634f028df6127dff2e6f8713aba3f86c790b
Hummad Khan <96032426+HummadKhan1@users.noreply.github.com>
2026-02-18 16:13:30 -0600

    Time: 27 ms (54.57%) | Memory: 20.5 MB (25.76%) - LeetSync

--- 2475-largest-palindromic-number/largest-palindromic-number.py
+++ 2475-largest-palindromic-number/largest-palindromic-number.py
@@ -2,28 +2,26 @@ from collections import Counter
 class Solution:
     def largestPalindromic(self, num: str) -> str:
         '''
-        parameters: str num.
+        parameters: string num.
         return: str largest palindromic integer.
-        Really asking: count_dict, sorted_unique, mid_found.
-        Constraints: NO LEADING ZEROES
+        Really asking: count_dict, sorted_unique, mid_found, new_str.
+        Constraints: no leading zeroes, return "0" if str and mid empty.
         '''
         count_dict = Counter(num)
-        unique = set(num)
-        sorted_unique = sorted(unique, reverse=True)
-        new_string = ""
+        sorted_unique = sorted(set(num), reverse=True)
         mid_found = False
-        mid = ""
-        for s in sorted_unique:
-            if count_dict[s]%2 == 0:
-                new_string += s*(count_dict[s]//2)
+        mid = ''
+        new_string = ''
+
+        for u in sorted_unique:
+            if count_dict[u]%2 == 0:
+                new_string += u*(count_dict[u]//2)
             else:
+                new_string += u*(count_dict[u]//2)
                 if not mid_found:
-                    mid = s
                     mid_found = True
-                    new_string += s*(count_dict[s]//2)
-                else:
-                    new_string += s*(count_dict[s]//2)
-        stripped_string = new_string.lstrip("0")
+                    mid = u
+        stripped_string = new_string.lstrip('0')
         if not stripped_string and not mid:
-            return "0"
+            return '0'
         return stripped_string + mid + stripped_string[::-1]
