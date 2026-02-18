70063620873e959b59d05ad793043431f8da1944
Hummad Khan <96032426+HummadKhan1@users.noreply.github.com>
2026-02-18 15:57:45 -0600

    Time: 31 ms (52.35%) | Memory: 20.3 MB (72.30%) - LeetSync

--- 2475-largest-palindromic-number/largest-palindromic-number.py
+++ 2475-largest-palindromic-number/largest-palindromic-number.py
@@ -2,34 +2,28 @@ from collections import Counter
 class Solution:
     def largestPalindromic(self, num: str) -> str:
         '''
-        parameters: string num (digits only).
-        return: string using digits from num.
+        parameters: str num.
+        return: str largest palindromic integer.
+        Really asking: count_dict, sorted_unique, mid_found.
+        Constraints: NO LEADING ZEROES
         '''
-        num_dict = Counter(num)
+        count_dict = Counter(num)
         unique = set(num)
-        new_string = ''
         sorted_unique = sorted(unique, reverse=True)
+        new_string = ""
         mid_found = False
-        mid = ''
-        res = ''
-        for u in sorted_unique:
-            count = num_dict[u]
-            if count%2 == 0:
-                new_string += u * (num_dict[u]//2)
+        mid = ""
+        for s in sorted_unique:
+            if count_dict[s]%2 == 0:
+                new_string += s*(count_dict[s]//2)
             else:
-                new_string += u * ((count-1)//2)
                 if not mid_found:
-                    mid = u
+                    mid = s
                     mid_found = True
-        new_string = new_string.lstrip('0')
-        if not new_string and not mid:
-            return '0'
-        res += new_string
-        if mid_found:
-            res += mid
-        res += new_string[::-1]
-        return res
-        
-            
-
-        
+                    new_string += s*(count_dict[s]//2)
+                else:
+                    new_string += s*(count_dict[s]//2)
+        stripped_string = new_string.lstrip("0")
+        if not stripped_string and not mid:
+            return "0"
+        return stripped_string + mid + stripped_string[::-1]
