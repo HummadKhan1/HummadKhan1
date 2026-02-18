8683df973ce6737602768c80b7001b2a55d81056
Hummad Khan <96032426+HummadKhan1@users.noreply.github.com>
2026-02-18 15:15:57 -0600

    Time: 7 ms (50.27%) | Memory: 19.6 MB (8.62%) - LeetSync

--- 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.py
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.py
@@ -1,15 +1,9 @@
 class Solution:
     def removeAnagrams(self, words: List[str]) -> List[str]:
-        '''
-        parameters: str arr.
-        return: words arr.
-        '''
-
-        new_list = [words[0]]
-
-        for i in range(1, len(words)):
+        res = [words[0]]
+        for i in range(1 ,len(words)):
             if sorted(words[i-1]) == sorted(words[i]):
                 continue
             else:
-                new_list.append(words[i])
-        return new_list
+                res.append(words[i])
+        return res
