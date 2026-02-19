527a099a12f9e4cd5f99bc8f9ed9b4c48ab508f1
Hummad Khan <96032426+HummadKhan1@users.noreply.github.com>
2026-02-19 15:50:29 -0600

    Time: 79 ms (67.38%) | Memory: 25.4 MB (39.50%) - LeetSync

--- 525-contiguous-array/contiguous-array.py
+++ 525-contiguous-array/contiguous-array.py
@@ -1,16 +1,22 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
-        prefixSums = {0:-1}
+        '''
+        Parameters: binary arr nums.
+        returns: max length of a contiguous subararray with equal num of 0s and 1s.
+        Really asking: prefix sum problem where if prefix exists in dict then it is a vaild subarray.
+        '''
+        num_dict = {0:-1}
         prefix = 0
-        res = 0
+        max_length = 0
+
         for i in range(len(nums)):
-            if nums[i] > 0:
-                prefix += 1
-            else:
+            if nums[i] == 0:
                 prefix -= 1
-            
-            if prefix in prefixSums:
-                res = max(res, i-prefixSums[prefix])
             else:
-                prefixSums[prefix] = i
-        return res
+                prefix += 1
+
+            if prefix in num_dict:
+                max_length = max(max_length, i-num_dict[prefix])
+            else:
+                num_dict[prefix] = i
+        return max_length
