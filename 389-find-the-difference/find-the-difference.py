413c9e55aa8f0ca51503d5e2483c7ac4d652cb7e
Hummad Khan <96032426+HummadKhan1@users.noreply.github.com>
2026-03-25 13:53:06 -0500

    Time: 0 ms (100.00%) | Memory: 19.3 MB (50.24%) - LeetSync

--- 389-find-the-difference/find-the-difference.py
+++ 389-find-the-difference/find-the-difference.py
@@ -1,10 +1,11 @@
 from collections import Counter
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        s_dict = Counter(s)
-        t_dict = Counter(t)
+        s_count = Counter(s)
+        t_count = Counter(t)
 
-        diff = t_dict - s_dict
+        diff = t_count - s_count
 
+        res = list(diff.keys())
 
-        return ''.join(list(diff.keys()))
+        return res[0]
