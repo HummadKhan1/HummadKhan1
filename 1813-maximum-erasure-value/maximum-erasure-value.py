282426caa9aa65560e73be397abb690d96c5d1ae
Hummad Khan <96032426+HummadKhan1@users.noreply.github.com>
2026-03-26 15:35:56 -0500

    Time: 155 ms (87.71%) | Memory: 32.4 MB (22.14%) - LeetSync

--- 1813-maximum-erasure-value/maximum-erasure-value.py
+++ 1813-maximum-erasure-value/maximum-erasure-value.py
@@ -1,16 +1,16 @@
 class Solution:
     def maximumUniqueSubarray(self, nums: List[int]) -> int:
-        dup_set = set()
-        L = 0
-        score = 0
         max_score = 0
-        
+        cur_score = 0
+        s_set = set()
+        L = 0
+
         for R in range(len(nums)):
-            while nums[R] in dup_set:
-                score -= nums[L]
-                dup_set.remove(nums[L])
+            while nums[R] in s_set:
+                s_set.remove(nums[L])
+                cur_score -= nums[L]
                 L += 1
-            dup_set.add(nums[R])
-            score += nums[R]
-            max_score = max(max_score, score)
+            s_set.add(nums[R])
+            cur_score += nums[R]
+            max_score = max(max_score, cur_score)
         return max_score
