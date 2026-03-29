27bfaa101882c872fdb75131c00a97da2de6c918
Hummad Khan <96032426+HummadKhan1@users.noreply.github.com>
2026-03-29 15:08:49 -0500

    Time: 0 ms (100.00%) | Memory: 19.5 MB (11.88%) - LeetSync

--- 2102-find-the-middle-index-in-array/find-the-middle-index-in-array.py
+++ 2102-find-the-middle-index-in-array/find-the-middle-index-in-array.py
@@ -6,13 +6,13 @@ class Solution:
             maxLeft.append(total)
             total += n
         
-        maxRight = [0]* len(nums)
+        maxRight = [0]*len(nums)
         total = 0
         for i in range(len(nums)-1,-1,-1):
             maxRight[i] = total
             total += nums[i]
         
         for i in range(len(nums)):
-            if maxLeft[i] == maxRight[i]:
+            if maxRight[i] == maxLeft[i]:
                 return i
         return -1
