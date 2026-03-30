dcda4a4423fe7c606407c86c13923f5ca87f7b6e
Hummad Khan <96032426+HummadKhan1@users.noreply.github.com>
2026-03-30 15:31:53 -0500

    Time: 107 ms (15.06%) | Memory: 38.1 MB (5.11%) - LeetSync

--- 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.py
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.py
@@ -1,24 +1,21 @@
 class Solution:
     def waysToSplitArray(self, nums: List[int]) -> int:
-        '''
-        parameters: int arr nums.
-        returns: NUMBER OF valid splits in nums.
-        Really asking: 
-        '''
         maxLeft = []
         total = 0
         for n in nums:
             total += n
             maxLeft.append(total)
-        
-        maxRight = [0]*len(nums)
+
+        maxRight = [0]* len(nums)
         total = 0
         for i in range(len(nums)-1,-1,-1):
             maxRight[i] = total
             total += nums[i]
         
         res = 0
-        for i in range(len(nums)-1):
-            if maxLeft[i] >= maxRight[i]:
+        for i in range(len(nums)):
+            if i == len(nums)-1:
+                continue
+            if maxRight[i] <= maxLeft[i]:
                 res += 1
         return res
