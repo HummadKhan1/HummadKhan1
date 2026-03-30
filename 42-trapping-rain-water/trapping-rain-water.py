e4e0baead4f85f999f95eb46dac49e438f21bb6c
Hummad Khan <96032426+HummadKhan1@users.noreply.github.com>
2026-03-30 13:23:48 -0500

    Time: 19 ms (17.64%) | Memory: 21 MB (91.22%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,9 +1,9 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
         '''
-        paramters: int arr height.
-        return: total amount of water that will stay.
-        Really asking: prefix sum problem where total amount of water that can be held in height[i] is = min(maxLeft[i], maxRight[i]) - height[i].
+        parameters: int height.
+        return: # of trapped tiles.
+        Actually asking: prefix sum problem. min(maxLeft[i], maxRight[i]) - height[i]
         '''
         maxLeft = []
         maxHeight = 0
@@ -16,10 +16,9 @@ class Solution:
         for i in range(len(height)-1,-1,-1):
             maxRight[i] = maxHeight
             maxHeight = max(maxHeight, height[i])
-        
-        total = 0
+
+        res = 0
         for i in range(len(height)):
-            diff = min(maxLeft[i], maxRight[i]) - height[i]
-            if diff > 0:
-                total += diff
-        return total
+            minHeight = min(maxLeft[i], maxRight[i])
+            res += max(0, minHeight-height[i])
+        return res
