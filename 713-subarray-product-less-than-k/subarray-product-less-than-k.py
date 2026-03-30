47d80209a21c4d643679988d2584ce33d8bee2ce
Hummad Khan <96032426+HummadKhan1@users.noreply.github.com>
2026-03-30 14:15:49 -0500

    Time: 59 ms (64.80%) | Memory: 22.2 MB (66.21%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.py
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.py
@@ -1,23 +1,21 @@
 class Solution:
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
         '''
-        parameters: arr of ints nums, int k
-        returns: NUMBER OF contiguous subarrays where the product < k.
-        Really asking: prefix sum problem.
-        Constraints: cannot divide by 0.
+        parameters: array of ints nums, int k.
+        return: NUMBER OF contiguous subarrays where product < k.
+        Really asking: Sliding Window 
         '''
         if k == 0:
             return 0
-        
+        L = 0
         cur_prod = 1
         res = 0
-        L = 0
-        
+
         for R in range(len(nums)):
             cur_prod *= nums[R]
             while cur_prod >= k and L <= R:
                 cur_prod //= nums[L]
                 L += 1
-            
-            res += R-L+1
+            res += (R-L+1)
+
         return res
