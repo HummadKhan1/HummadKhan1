58db2183d095c3ad4bdb0bbdb466648c5ab1a460
Hummad Khan <96032426+HummadKhan1@users.noreply.github.com>
2026-03-30 15:45:38 -0500

    Time: 47 ms (94.89%) | Memory: 39.6 MB (36.10%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.py
+++ 523-continuous-subarray-sum/continuous-subarray-sum.py
@@ -1,23 +1,14 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        '''
-        parameters: int arr nums, pos int k.
-        return: boolean true or false
-        constraints: 
-        Really asking: (nums[i]-nums[j]) % k == 0
-                       (nums[i] % k) - (nums[j] % k) == 0
-                       nums[i] % k == nums[j] % k
-        If we can find a 
-        '''
-        prefixSum = {0:-1}
+        prefix_sums = {0:-1}
         prefix = 0
         for i in range(len(nums)):
             prefix += nums[i]
-            prefix  = prefix % k
-            
-            if prefix in prefixSum:
-                if i-prefixSum[prefix] >= 2:
+            remainder = prefix % k
+
+            if remainder in prefix_sums:
+                if i-prefix_sums[remainder] >= 2:
                     return True
             else:
-                prefixSum[prefix] = i
+                prefix_sums[remainder] = i
         return False
