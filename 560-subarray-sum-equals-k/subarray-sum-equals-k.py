6eb4fd734866fdff99cb73b3da4c2ded10d3d61d
Hummad Khan <96032426+HummadKhan1@users.noreply.github.com>
2026-03-30 13:42:07 -0500

    Time: 24 ms (96.46%) | Memory: 21.9 MB (72.23%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,19 +1,22 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         '''
-        parameters: arr of int nums.
-        return: total number of subarrays with sum of k.
-        Really asking: Use dict to keep track of sums in current index. If cur_sum - k is in dict then that is a valid subarray.
+        parameters: array of ints, int k.
+        return: NUMBER OF subarrays whose sum equals to k.
+        Really asking: Prefix sum. 
         '''
-        sum_dict = {0:1}
+
+        nums_dict = {0:1}
         cur_sum = 0
         res = 0
+        
         for n in nums:
             cur_sum += n
             prefix = cur_sum - k
 
-            if prefix in sum_dict:
-                res += sum_dict.get(prefix)
+            if prefix in nums_dict:
+                res += nums_dict[prefix]
             
-            sum_dict[cur_sum] = sum_dict.get(cur_sum, 0)+1
+            nums_dict[cur_sum] = nums_dict.get(cur_sum, 0) + 1
+        
         return res
