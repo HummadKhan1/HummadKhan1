2a2b74ca799afbc48a509442c69cd4d9e31b7d6f
Hummad Khan <96032426+HummadKhan1@users.noreply.github.com>
2026-03-30 15:12:40 -0500

    Time: 96 ms (39.05%) | Memory: 31.6 MB (43.08%) - LeetSync

--- 878-shifting-letters/shifting-letters.py
+++ 878-shifting-letters/shifting-letters.py
@@ -1,17 +1,17 @@
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[int]) -> str:
         '''
-        parameters: string s, int arr shifts.
-        returns: final string afterall shifts are applied.
-        Really asking: prefix sums
+        parameters: string s, int arr shifts. 
+        returns: string.
+        Really asking: Iterate backwards. chr() and ord().
         constraints:
         '''
-        new_str = ""
         total_shifts = 0
+        new_str = ""
         for i in range(len(s)-1,-1,-1):
             total_shifts += shifts[i]
-            original = ord(s[i])-ord('a')
-            original = (original+total_shifts) % 26
-            new_letter = chr(original+ord('a'))
-            new_str += new_letter
+            original = ord(s[i]) - ord('a')
+            new_chr = (original + total_shifts) % 26
+            new_chr = new_chr + ord('a')
+            new_str += chr(new_chr)
         return new_str[::-1]
