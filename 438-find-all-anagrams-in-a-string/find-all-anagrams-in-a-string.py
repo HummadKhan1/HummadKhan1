7a2e44f0d610601e325798f41120f1ad0685a045
Hummad Khan <96032426+HummadKhan1@users.noreply.github.com>
2026-04-14 15:30:14 -0500

    Time: 130 ms (38.26%) | Memory: 19.9 MB (49.61%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,13 +1,23 @@
 from collections import Counter
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
+        '''
+        parameters: string s, string p.
+        returns: array.
+        Really asking: get len(dict) == len(p). add s[R]+=1 and s[L]-=1
+        Variables: s_dict, L, R, res.
+        Constraints: 
+        '''
         s_dict = {}
         p_dict = Counter(p)
         L = 0
         res = []
+
         for R in range(len(s)):
             s_dict[s[R]] = s_dict.get(s[R], 0)+1
-            while (R-L+1) > len(p):
+            if R-L+1 < sum(p_dict.values()):
+                continue
+            while R-L+1 > sum(p_dict.values()):
                 s_dict[s[L]] -= 1
                 if s_dict[s[L]] == 0:
                     del s_dict[s[L]]
