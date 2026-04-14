051b8a97b067aa5cfadacfc76abbe836a8717b5b
Hummad Khan <96032426+HummadKhan1@users.noreply.github.com>
2026-04-14 14:08:42 -0500

    Time: 146 ms (26.30%) | Memory: 19.7 MB (60.98%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,14 +1,22 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        '''
+        parameters: string s, int k.
+        returns: LENGTH of longest substring.
+        Really asking: Sliding Window, while max(dict.values()) <= k.
+        Variables: s_dict, L, R, max_len.
+        Constraints: k could be 0:
+        '''
         s_dict = {}
         L = 0
         max_len = 0
+
         for R in range(len(s)):
             s_dict[s[R]] = s_dict.get(s[R], 0)+1
-            while (R-L+1) - max(s_dict.values()) > k:
+            while (R-L+1)-max(s_dict.values()) > k:
                 s_dict[s[L]] -= 1
                 if s_dict[s[L]] == 0:
                     del s_dict[s[L]]
                 L += 1
-            max_len = max(max_len, R-L+1)
+            max_len = max(max_len, (R-L+1))
         return max_len
