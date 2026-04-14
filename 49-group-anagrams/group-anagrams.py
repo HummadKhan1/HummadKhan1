f01a800ab411b7cb50dc2f36716ef239b4407b9b
Hummad Khan <96032426+HummadKhan1@users.noreply.github.com>
2026-04-14 13:15:05 -0500

    Time: 12 ms (58.46%) | Memory: 21.8 MB (94.02%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,5 +1,12 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        '''
+        Parameters: array of str strs.
+        returns: list in list.
+        Really asking: group anagrams problem. 
+        Variables: group_dict, group_list, sorted_word.
+        Constraints: all lowercase, element could be "".
+        '''
         group_dict = {}
 
         for word in strs:
@@ -8,4 +15,5 @@ class Solution:
                 group_dict[sorted_word].append(word)
             else:
                 group_dict[sorted_word] = [word]
-        return list(group_dict.values()) 
+        group_list = list(group_dict.values())
+        return group_list
