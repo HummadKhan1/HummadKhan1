b699fa98c93605f826c271f6c092d03346f87ced
Hummad Khan <96032426+HummadKhan1@users.noreply.github.com>
2026-04-14 15:44:35 -0500

    Time: 31 ms (37.54%) | Memory: 19.3 MB (89.62%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -4,14 +4,13 @@ class Solution:
         s1_dict = Counter(s1)
         s2_dict = {}
         L = 0
-
         for R in range(len(s2)):
             s2_dict[s2[R]] = s2_dict.get(s2[R], 0)+1
-            while (R-L+1) > len(s1):
+            while R-L+1 > len(s1):
                 s2_dict[s2[L]] -= 1
                 if s2_dict[s2[L]] == 0:
                     del s2_dict[s2[L]]
                 L += 1
-            if s2_dict == s1_dict:
+            if s1_dict == s2_dict:
                 return True
         return False
